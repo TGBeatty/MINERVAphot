b99b6c4b123b823ddcf0326f6fb949a672df9e94
TGBeatty <tgbeatty@gmail.com>
2015-09-04 17:21:13 -0400

    multi-phot not working

--- MINERVAphot.py
+++ MINERVAphot.py
@@ -1,109 +1,120 @@
-import numpy, progressbar, astropy, math, sys, os
+import numpy, progressbar, astropy, math, sys, os, aplpy
 import photutils as pu
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
 
-#comment
-
-def MakeBias(fitslist, outloc):
+def MakeBias(fitslist, outloc, skip=False):
 	if not os.path.exists(outloc):
 		os.makedirs(outloc)
 
 	nimages = len(fitslist)
-
-	print "Making bias image..."
-	bar = progressbar.ProgressBar(maxval=nimages, widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
-	for i in range(nimages):
-		hdulist = astropy.io.fits.open(fitslist[i])
-		if i == 0:
-			size = (nimages,hdulist[0].data.shape[0],hdulist[0].data.shape[1])
-			tomedian = numpy.zeros(size)
-		tomedian[i,:,:] = hdulist[0].data
-		bar.update(i+1)
-	bar.finish()
-
-	hdulist[0].data = numpy.median(tomedian, axis=0)
 	biasloc = outloc+"mbias.fits"
-	hdulist.writeto(biasloc, clobber=True)
+
+	if not skip:
+		print "Making bias image..."
+		bar = progressbar.ProgressBar(maxval=nimages, widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
+		for i in range(nimages):
+			hdulist = astropy.io.fits.open(fitslist[i])
+			if i == 0:
+				size = (nimages,hdulist[0].data.shape[0],hdulist[0].data.shape[1])
+				tomedian = numpy.zeros(size)
+			tomedian[i,:,:] = hdulist[0].data
+			bar.update(i+1)
+		bar.finish()
+
+		hdulist[0].data = numpy.median(tomedian, axis=0)
+		hdulist.writeto(biasloc, clobber=True)
 
 	return biasloc
 
-def MakeDark(fitslist, outloc, mbiasloc):
+def MakeDark(fitslist, outloc, mbiasloc, skip=False):
 	if not os.path.exists(outloc):
 		os.makedirs(outloc)
 
 	nimages = len(fitslist)
-	hdulist = astropy.io.fits.open(mbiasloc)
-	bias = hdulist[0].data
-
-	print "Making dark image..."
-	bar = progressbar.ProgressBar(maxval=nimages, widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
-	for i in range(nimages):
-		hdulist = astropy.io.fits.open(fitslist[i])
-		if i == 0:
-			size = (nimages,hdulist[0].data.shape[0],hdulist[0].data.shape[1])
-			tomedian = numpy.zeros(size)
-		tomedian[i,:,:] = hdulist[0].data - bias
-		bar.update(i+1)
-	bar.finish()
-
-	hdulist[0].data = numpy.median(tomedian, axis=0)
 	darkloc = outloc+"mdark.fits"
-	hdulist.writeto(darkloc, clobber=True)
+
+	if not skip:
+		hdulist = astropy.io.fits.open(mbiasloc)
+		bias = hdulist[0].data
+
+		print "Making dark image..."
+		bar = progressbar.ProgressBar(maxval=nimages, widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
+		for i in range(nimages):
+			hdulist = astropy.io.fits.open(fitslist[i])
+			if i == 0:
+				size = (nimages,hdulist[0].data.shape[0],hdulist[0].data.shape[1])
+				tomedian = numpy.zeros(size)
+			tomedian[i,:,:] = hdulist[0].data - bias
+			bar.update(i+1)
+		bar.finish()
+
+		hdulist[0].data = numpy.median(tomedian, axis=0)
+		hdulist.writeto(darkloc, clobber=True)
 
 	return darkloc
 
-def MakeFlat(fitslist, outloc, mbiasloc, mdarkloc):
+def MakeFlat(fitslist, outloc, mbiasloc, mdarkloc, skip=False):
 	if not os.path.exists(outloc):
 		os.makedirs(outloc)
 
 	nimages = len(fitslist)
-	hdulist = astropy.io.fits.open(mbiasloc)
-	bias = hdulist[0].data
-	hdulist = astropy.io.fits.open(mdarkloc)
-	dark = hdulist[0].data
-
-	print "Making flat image..."
-	bar = progressbar.ProgressBar(maxval=nimages, widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
-	for i in range(nimages):
-		hdulist = astropy.io.fits.open(fitslist[i])
-		if i == 0:
-			size = (nimages,hdulist[0].data.shape[0],hdulist[0].data.shape[1])
-			tomedian = numpy.zeros(size)
-		tomedian[i,:,:] = (hdulist[0].data - bias - dark) / numpy.median(hdulist[0].data)
-		bar.update(i+1)
-	bar.finish()
-
-	hdulist[0].data = numpy.median(tomedian, axis=0)
 	flatloc = outloc+"mflat.fits"
-	hdulist.writeto(flatloc, clobber=True)
+
+	if not skip:
+		hdulist = astropy.io.fits.open(mbiasloc)
+		bias = hdulist[0].data
+		hdulist = astropy.io.fits.open(mdarkloc)
+		dark = hdulist[0].data
+
+		print "Making flat image..."
+		bar = progressbar.ProgressBar(maxval=nimages, widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
+		for i in range(nimages):
+			hdulist = astropy.io.fits.open(fitslist[i])
+			if i == 0:
+				size = (nimages,hdulist[0].data.shape[0],hdulist[0].data.shape[1])
+				tomedian = numpy.zeros(size)
+			tomedian[i,:,:] = (hdulist[0].data - bias - dark) / numpy.median(hdulist[0].data)
+			bar.update(i+1)
+		bar.finish()
+
+		hdulist[0].data = numpy.median(tomedian, axis=0)
+		hdulist.writeto(flatloc, clobber=True)
 
 	return flatloc
 
-def BiasDarkFlat(fitslist, outloc, mbiasloc, mdarkloc, mflatloc):
+def BiasDarkFlat(fitslist, outloc, mbiasloc, mdarkloc, mflatloc, skip=False):
 	if not os.path.exists(outloc):
 		os.makedirs(outloc)
 
 	nimages = len(fitslist)
 	procimgloc = numpy.empty(nimages, dtype='object') # What will be the list of processed images
 
-	hdulist = astropy.io.fits.open(mbiasloc)
-	bias = hdulist[0].data
-	hdulist = astropy.io.fits.open(mdarkloc)
-	dark = hdulist[0].data
-	hdulist = astropy.io.fits.open(mflatloc)
-	flat = hdulist[0].data 
+	if not skip:
+		hdulist = astropy.io.fits.open(mbiasloc)
+		bias = hdulist[0].data
+		hdulist = astropy.io.fits.open(mdarkloc)
+		dark = hdulist[0].data
+		hdulist = astropy.io.fits.open(mflatloc)
+		flat = hdulist[0].data 
 
-	print "Calibrating images..."
-	bar = progressbar.ProgressBar(maxval=nimages, widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
-	for i in range(nimages):
-		hdulist = astropy.io.fits.open(fitslist[i])
-		image = hdulist[0].data
-		hdulist[0].data = (image - bias - dark) / flat
-		splitname = fitslist[i].split('/')
-		filenamesplit = splitname[-1].partition('.fit')
-		procimgloc[i] = outloc+filenamesplit[0]+".proc"+filenamesplit[1]+filenamesplit[2]
-		hdulist.writeto(procimgloc[i], clobber=True)
-		bar.update(i+1)
-	bar.finish()
+		print "Calibrating images..."
+		bar = progressbar.ProgressBar(maxval=nimages, widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
+		for i in range(nimages):
+			hdulist = astropy.io.fits.open(fitslist[i])
+			image = hdulist[0].data
+			hdulist[0].data = (image - bias - dark) / flat
+			splitname = fitslist[i].split('/')
+			filenamesplit = splitname[-1].partition('.fit')
+			procimgloc[i] = outloc+filenamesplit[0]+".proc"+filenamesplit[1]+filenamesplit[2]
+			hdulist.writeto(procimgloc[i], clobber=True)
+			bar.update(i+1)
+		bar.finish()
+	else:
+		for i in range(nimages):
+			splitname = fitslist[i].split('/')
+			filenamesplit = splitname[-1].partition('.fit')
+			procimgloc[i] = outloc+filenamesplit[0]+".proc"+filenamesplit[1]+filenamesplit[2]
 
 	return procimgloc
 
@@ -127,10 +138,7 @@ def HowellCenter(image, center, L):
 
 	return centerloc
 
-def SingleAper(fitslist, rphot, rbkgin, rbkgout):
-	rastar = 348.49483333
-	decstar = 8.76127777
-	L = 5. # centering box half size
+def SingleAper(fitslist, AperProps):
 
 	nimages = len(fitslist)
 	jdutc = numpy.zeros(nimages)
@@ -154,20 +162,30 @@ def SingleAper(fitslist, rphot, rbkgin, rbkgout):
 		try:
 			wcscheck = hdulist[0].header['WCSAXES']
 			w = astropy.wcs.WCS(hdulist[0].header)
-			WCScenter = w.wcs_world2pix(rastar, decstar, 0)
+			WCScenter = w.wcs_world2pix(AperProps['rastar']*15., AperProps['decstar'], 0)
 		except KeyError: # If there is no WCS solution
 			try:
 				WCScenter = WCScenter # default to the previous WCS center location
 			except NameError: # but, if it's the first image, stop
 				print "Initial image has no WCS solution! Stopping."
 				sys.exit()
+		if (WCScenter[0]<0.) or (WCScenter[0]>2048.): 
+			print "The WCS solution has given an invalid pixel location! Stopping."
+			print WCScenter
+			print "Image name is:", fitslist[i]
+			sys.exit()
+		if (WCScenter[1]<0.) or (WCScenter[1]>2048.): 
+			print "The WCS solution has given an invalid pixel location! Stopping."
+			print WCScenter
+			print "Image name is:", fitslist[i]
+			sys.exit()
 
-		centerloc = HowellCenter(image, WCScenter, L)
+		centerloc = HowellCenter(image, WCScenter, AperProps['L'])
 		xcenter[i] = centerloc[0]
 		ycenter[i] = centerloc[1]
 
-		photaper = pu.CircularAperture(centerloc, r=rphot)
-		bkgaper = pu.CircularAnnulus(centerloc, r_in=rbkgin, r_out=rbkgout)
+		photaper = pu.CircularAperture(centerloc, r=AperProps['rAp'])
+		bkgaper = pu.CircularAnnulus(centerloc, r_in=AperProps['rBkgIn'], r_out=AperProps['rBkgOut'])
 		raw_table = pu.aperture_photometry(image, photaper)
 		bkg_table = pu.aperture_photometry(image, bkgaper)
 		bkgmean = bkg_table['aperture_sum'] / bkgaper.area()
@@ -179,3 +197,100 @@ def SingleAper(fitslist, rphot, rbkgin, rbkgout):
 
 	toreturn = numpy.column_stack((jdutc, flux, error, airmass, xcenter, ycenter))
 	return toreturn
+
+def MultiAper(fitslist, AperProps):
+
+	nstars = AperProps['rastar'].size
+	RAlist = AperProps['rastar']
+	Declist = AperProps['decstar']
+
+	nimages = len(fitslist)
+	jdutc = numpy.zeros(nimages)
+	flux = numpy.zeros((nstars,nimages))
+	error = numpy.zeros((nstars,nimages))
+	xcenter = numpy.zeros((nstars,nimages))
+	ycenter = numpy.zeros((nstars,nimages))
+	airmass = numpy.zeros(nimages)
+
+	print "Extracting photometry..."
+	bar = progressbar.ProgressBar(maxval=nimages, widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
+	for i in range(nimages):
+		hdulist = astropy.io.fits.open(fitslist[i])
+		image = hdulist[0].data 
+		starttime = numpy.float(hdulist[0].header['JD'])
+		exptime = numpy.float(hdulist[0].header['EXPTIME'])
+		jdutc[i] = starttime + ((exptime/2.)/(24.*60*60.))
+		alt = numpy.float(hdulist[0].header['OBJCTALT'])*math.pi/180.
+		airmass[i] = 1./numpy.cos((math.pi/2.)-alt)
+
+		for j in range(nstars):
+			try:
+				wcscheck = hdulist[0].header['WCSAXES']
+				w = astropy.wcs.WCS(hdulist[0].header)
+				WCScenter = w.wcs_world2pix(RAlist[j]*15., Declist[j], 0)
+			except KeyError: # If there is no WCS solution
+				try:
+					WCScenter = WCScenter # default to the previous WCS center location
+				except NameError: # but, if it's the first image, stop
+					print "Initial image has no WCS solution! Stopping."
+					sys.exit()
+			if (WCScenter[0]<0.) or (WCScenter[0]>2048.): 
+				print "The WCS solution has given an invalid pixel location! Stopping."
+				print WCScenter
+				print "Image name is:", fitslist[i]
+				sys.exit()
+			if (WCScenter[1]<0.) or (WCScenter[1]>2048.): 
+				print "The WCS solution has given an invalid pixel location! Stopping."
+				print WCScenter
+				print "Image name is:", fitslist[i]
+				sys.exit()
+
+			centerloc = HowellCenter(image, WCScenter, AperProps['L'])
+			xcenter[j,i] = centerloc[0]
+			ycenter[j,i] = centerloc[1]
+
+			photaper = pu.CircularAperture(centerloc, r=AperProps['rAp'])
+			bkgaper = pu.CircularAnnulus(centerloc, r_in=AperProps['rBkgIn'], r_out=AperProps['rBkgOut'])
+			raw_table = pu.aperture_photometry(image, photaper)
+			bkg_table = pu.aperture_photometry(image, bkgaper)
+			bkgmean = bkg_table['aperture_sum'] / bkgaper.area()
+			photbkg = bkgmean*photaper.area()
+			flux[j,i] = raw_table['aperture_sum'] - photbkg
+			error[j,i] = numpy.sqrt(raw_table['aperture_sum'] + photbkg)
+		bar.update(i+1)
+	bar.finish()
+
+	toreturn = numpy.column_stack((jdutc, flux, error, airmass, xcenter, ycenter))
+	return toreturn
+
+def on_click(event):
+	global PickedLocsPix, w, fig
+
+	if event.inaxes is not None:
+		WCSloc = w.wcs_pix2world(event.xdata, event.ydata, 0)
+		fig.show_markers(WCSloc[0], WCSloc[1])
+		temp = [event.xdata, event.ydata]
+		PickedLocsPix = numpy.vstack((PickedLocsPix,temp))
+		fig.refresh()
+	else:
+		print 'Clicked ouside axes bounds but inside plot window'
+
+def GraphicalPickLocs(imagename):
+	global PickedLocsPix, w, fig
+
+	PickedLocsPix = numpy.zeros(2)
+	mplfig = plt.figure()
+	hdulist = astropy.io.fits.open(imagename)
+	w = astropy.wcs.WCS(hdulist[0].header)
+	fig = aplpy.FITSFigure(imagename, figure=mplfig)
+	fig.show_grayscale()
+	fig.set_xaxis_coord_type('longitude')
+	fig.set_yaxis_coord_type('latitude')
+	fig.tick_labels.set_xformat('dd.dddddd')
+	fig.tick_labels.set_yformat('dd.dddddd')
+	mplfig.canvas.callbacks.connect('button_press_event', on_click)
+	plt.show()
+	PickedLocsPix = PickedLocsPix[1:]
+	PickedLocsWCS = w.wcs_pix2world(PickedLocsPix[:,0], PickedLocsPix[:,1], 0)
+
+	return PickedLocsWCS
--- singlephot.py
+++ singlephot.py
@@ -1,27 +1,46 @@
-import numpy
+import numpy, sys, aplpy, astropy
 import matplotlib.pyplot as plt
 import MINERVAphot as mp
 
-rphotap = 12
-rbkgin = 15
-rbkgout = 20
+def on_click(event):
+    if event.inaxes is not None:
+    	WCSloc = w.wcs_pix2world(event.xdata, event.ydata, 0)
+    	fig.show_markers(WCSloc[0], WCSloc[1])
+    	fig.refresh()
+    else:
+        print 'Clicked ouside axes bounds but inside plot window'
+
+AperProperties = {}
+AperProperties['rastar'] = 23.2329888 # decimal hours
+AperProperties['decstar'] = 8.76127777 # decimal deg.
+AperProperties['L'] = 5 # Half-size of the box for centering
+AperProperties['rAp'] = 12 # radius of the phot aperture
+AperProperties['rBkgIn'] = 15 # inner radius of the bkg annulus
+AperProperties['rBkgOut'] = 20 # outer radius of the bkg annulus
 
 biaslist = numpy.loadtxt('biases.list', dtype='object')
 darklist = numpy.loadtxt('darks.list', dtype='object')
 flatlist = numpy.loadtxt('flats.list', dtype='object')
 unproclist = numpy.loadtxt('unreduced.list', dtype='object')
 
-biasloc = mp.MakeBias(biaslist, './CalibFiles/')
-darkloc = mp.MakeDark(darklist, './CalibFiles/', biasloc)
-flatloc = mp.MakeFlat(flatlist, './CalibFiles/', biasloc, darkloc)
-proclist = mp.BiasDarkFlat(unproclist, './Processed/', biasloc, darkloc, flatloc)
+CalibFileSaveLoc = './CalibFiles/'
 
-photresults = mp.SingleAper(proclist, rphotap, rbkgin, rbkgout)
+biasloc = mp.MakeBias(biaslist, CalibFileSaveLoc, skip=True)
+darkloc = mp.MakeDark(darklist, CalibFileSaveLoc, biasloc, skip=True)
+flatloc = mp.MakeFlat(flatlist, CalibFileSaveLoc, biasloc, darkloc, skip=True)
+proclist = mp.BiasDarkFlat(unproclist, './Processed/', biasloc, darkloc, flatloc, skip=True)
+
+StarLocs = mp.GraphicalPickLocs(proclist[0])
+AperProperties['rastar'] = StarLocs[0]/15. # to decimal degress
+AperProperties['decstar'] = StarLocs[1]
+
+photresults = mp.SingleAper(proclist, AperProperties)
+
+#photresults = mp.MultiAper(proclist, AperProperties)
+
+print photresults
 
 numpy.savetxt('wasp52.lc', photresults)
-
-#fitslist = numpy.loadtxt('wasp52.list', dtype='object')
-#OGphotresults = minphot.SingleAper(fitslist, rphotap, rbkgin, rbkgout)
 
 plt.plot(photresults[:,0], photresults[:,1], '.')
 plt.show()
